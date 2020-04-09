a051eabae65aa3e2b5823db5c84cad4d43544133
Andresito de Guzman <andresitomyemail@gmail.com>
2020-04-10 02:16:47 +0800

    feat(app): update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaintenanceComponent } from './page/maintenance/maintenance.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaintenanceComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
