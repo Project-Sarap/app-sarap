9121419aeed905125913a37038fc2216eae49806
Andresito de Guzman <andresitomyemail@gmail.com>
2020-04-10 02:18:33 +0800

    feat(app): update app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MaintenanceComponent } from './page/maintenance/maintenance.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MaintenanceComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
