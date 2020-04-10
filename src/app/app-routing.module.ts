6b41624b0621731b507d359861e68d2bef8bda70
Andresito de Guzman <andresitomyemail@gmail.com>
2020-04-11 01:30:12 +0800

    feat(app): add rotue for preview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MaintenanceComponent } from './page/maintenance/maintenance.component';
 import { NotFoundComponent } from './page/not-found/not-found.component';
+import { PreviewComponent } from './page/preview/preview.component';
 
 const routes: Routes = [
   { path: '', component: MaintenanceComponent },
+  { path: 'preview', component: PreviewComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
